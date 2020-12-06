36dc05955de27bc555c962c13ccf07ce539b51c6
shihastz <shihast0503@gmail.com>
2020-12-06 18:42:59 +0530

    Updates

---
+++ precache-manifest.5ba081d987dae81bb35e2a38ebdb6a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27596344be22139b8055f5a37e93fea",
+    "url": "/food-recipe-app/index.html"
+  },
+  {
+    "revision": "4a3c5e0ce7ea931952a8",
+    "url": "/food-recipe-app/static/css/main.722e2ad0.chunk.css"
+  },
+  {
+    "revision": "cdc79fb2865a0d9e3a17",
+    "url": "/food-recipe-app/static/js/2.94c5326c.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/food-recipe-app/static/js/2.94c5326c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3c5e0ce7ea931952a8",
+    "url": "/food-recipe-app/static/js/main.3446f0a2.chunk.js"
+  },
+  {
+    "revision": "646cf2e3faa5980ba933",
+    "url": "/food-recipe-app/static/js/runtime-main.eba00d24.js"
+  }
+]);
